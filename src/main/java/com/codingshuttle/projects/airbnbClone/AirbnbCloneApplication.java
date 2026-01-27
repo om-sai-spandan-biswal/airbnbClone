db778c6b213531a5d233ed527d5363a3be1f805f
Om Sai Spandan Biswal <biswalomsaispandan@gmail.com>
2026-01-27 14:13:28 +0530

    just init

---
+++ src/main/java/com/codingshuttle/projects/airbnbClone/AirbnbCloneApplication.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.projects.airbnbClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnbCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingshuttle/projects/airbnbClone/AirbnbCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.projects.airbnbClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
